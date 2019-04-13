39eddb6a3304090965429211cb01be8bc790659d
Stepan Legachev <siberex@gmail.com>
2019-04-13 23:55:30 +0300

    reformatted code

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,23 +3,23 @@ import { Logger } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
-const PORT: string|number = process.env.PORT || 8080;
+const PORT: string | number = process.env.PORT || 8080;
 
 (async () => {
-  const app = await NestFactory.create<NestExpressApplication>(
-      AppModule,
-  );
+    const app = await NestFactory.create<NestExpressApplication>(
+        AppModule,
+    );
 
-  // Substitute real user IP from load balancer
-  app.set('trust proxy', true);
-  app.set('x-powered-by', false);
+    // Substitute real user IP from load balancer
+    app.set('trust proxy', true);
+    app.set('x-powered-by', false);
 
-  await app.listen(PORT, () => {
-    Logger.log(`App listening on port ${PORT}`, 'HTTP');
-    Logger.log('Press Ctrl+C to quit.', 'HTTP');
-  });
+    await app.listen(PORT, () => {
+        Logger.log(`App listening on port ${PORT}`, 'HTTP');
+        Logger.log('Press Ctrl+C to quit.', 'HTTP');
+    });
 })().catch(err => {
-  process.exitCode = 1;
-  // tslint:disable-next-line:no-console
-  console.error(err);
+    process.exitCode = 1;
+    // tslint:disable-next-line:no-console
+    console.error(err);
 });
