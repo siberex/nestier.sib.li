096576ec07cb2cf97a16524ee65868363b6431a0
Stepan Legachev <siberex@gmail.com>
2019-04-13 22:35:19 +0300

    adding basic modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksController } from './books/books.controller';
-import { BooksService } from './books/books.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, BooksController, UsersController],
-  providers: [AppService, BooksService, UsersService],
+  imports: [UsersModule, BooksModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
