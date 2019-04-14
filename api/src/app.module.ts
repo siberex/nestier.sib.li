67e8f3720a0d39554174f1d4585eb31ce7e63089
Stepan Legachev <siberex@gmail.com>
2019-04-14 11:33:17 +0300

    moved db options declaration inside config module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,19 +11,8 @@ import { ConfigModule, ConfigService } from './config';
     imports: [
         ConfigModule,
         TypeOrmModule.forRootAsync({
-            inject: [ConfigService],
-            useFactory: async (config: ConfigService) => ({
-                type: 'postgres',
-                host: config.get('POSTGRES_HOST'),
-                port: config.get('POSTGRES_PORT'),
-                username: config.get('POSTGRES_USER'),
-                password: config.get('POSTGRES_PASSWORD'),
-                database: config.get('POSTGRES_DB'),
-                entities: [__dirname + '/**/*.entity{.ts,.js}'],
-                synchronize: config.isDev(),
-                ssl: config.isProd(),
-            }),
-        } as TypeOrmModuleOptions),
+            useExisting: ConfigService,
+        }),
         UsersModule,
         BooksModule,
     ],
--- api/src/config/config.service.ts
+++ api/src/config/config.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Injectable()
-export class ConfigService {
+export class ConfigService implements TypeOrmOptionsFactory {
     private readonly config: { [key: string]: string | number };
     private readonly environment: string;
 
@@ -30,5 +31,19 @@ export class ConfigService {
             return process.env[key];
         }
         return undefined;
+    }
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            host: this.get('POSTGRES_HOST') as string || 'localhost',
+            port: this.get('POSTGRES_PORT') as number || 5432,
+            username: this.get('POSTGRES_USER') as string || 'postgres',
+            password: this.get('POSTGRES_PASSWORD') as string || '',
+            database: this.get('POSTGRES_DB') as string || 'test',
+            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            synchronize: this.isDev(),
+            ssl: this.isProd(),
+        };
     }
 }
