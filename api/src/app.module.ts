cf15b9d279e37585fc03a831b6f188df79e098c5
Stepan Legachev <siberex@gmail.com>
2019-04-14 06:08:53 +0300

    added DB connection parameters

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 
 @Module({
-    imports: [UsersModule, BooksModule],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST || 'localhost',
+            port: (process.env.POSTGRES_PORT as unknown as number) || 5432,
+            username: process.env.POSTGRES_USER || 'postgres',
+            password: process.env.POSTGRES_PASSWORD || '',
+            database: process.env.POSTGRES_DB || 'test',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+        }),
+        UsersModule,
+        BooksModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
