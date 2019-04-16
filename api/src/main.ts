bd8dcf481a617231a4b8588a02e31ca29691c6ce
Stepan Legachev <siberex@gmail.com>
2019-04-16 20:31:35 +0300

    fixed Swagger path for deployed app

--- api/src/main.ts
+++ api/src/main.ts
@@ -12,6 +12,11 @@ const PORT: string | number = process.env.API_PORT || process.env.PORT || 8081;
         AppModule,
     );
 
+    if ('GAE_SERVICE' in process.env && process.env.GAE_SERVICE === 'api') {
+        // Reflects dispatch.yaml for App Engine deployment
+        app.setGlobalPrefix('api');
+    }
+
     // Substitute real user IP from load balancer
     app.set('trust proxy', true);
     app.set('x-powered-by', false);
@@ -26,15 +31,15 @@ const PORT: string | number = process.env.API_PORT || process.env.PORT || 8081;
     // Add Swagger api doc
     const swaggerOptions = new DocumentBuilder()
         .setTitle('Nestier API')
-        .setVersion('1.0')
-        .build();
-    const apiDoc = SwaggerModule.createDocument(app, swaggerOptions);
-    SwaggerModule.setup('doc', app, apiDoc);
-
+        .setVersion('1.0');
+    let swaggerPath = 'doc';
     if ('GAE_SERVICE' in process.env && process.env.GAE_SERVICE === 'api') {
-        // Reflects dispatch.yaml for App Engine deployment
-        app.setGlobalPrefix('api');
+        swaggerOptions.setBasePath('api');
+        // Path should be set explicitly: https://github.com/nestjs/swagger/issues/105
+        swaggerPath = 'api/doc';
     }
+    const apiDoc = SwaggerModule.createDocument(app, swaggerOptions.build());
+    SwaggerModule.setup(swaggerPath, app, apiDoc);
 
     await app.listen(PORT, () => {
         Logger.log(`App listening on port ${PORT}`, 'HTTP');
