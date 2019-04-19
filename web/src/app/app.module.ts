4bb0016b259bdd3a7b2afd041f3f2b651cea1664
Stepan Legachev <siberex@gmail.com>
2019-04-19 08:13:09 +0300

    adding server-side boilerplate code

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WindowService } from './services/window/window.service';
+
+// For AoT compilation:
+export function getWindow() {
+  return window;
+}
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule,
+    HttpClientModule,
+    // Add .withServerTransition() to support Universal rendering.
+    // The application ID can be any identifier which is unique on
+    // the page.
+    BrowserModule.withServerTransition({appId: 'my-app'}),
+    TransferHttpCacheModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: WindowService,
+      useFactory: getWindow,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
