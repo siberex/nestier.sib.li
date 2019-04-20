1d57733629d69f46243c993f83895233434e2d72
Stepan Legachev <siberex@gmail.com>
2019-04-20 08:20:42 +0300

    added ssr-related code

--- web/server/app.controller.ts
+++ web/server/app.controller.ts
@@ -1,13 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {
-  }
-
-  @Get()
-  getIndex(): string {
-    return this.appService.getIndex();
-  }
-}
+export class AppController {}
