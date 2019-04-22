dc48c89db9a746d3feebd33763520df8f57471c9
Stepan Legachev <siberex@gmail.com>
2019-04-23 02:15:49 +0300

    adding routes

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './components/books/books.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserLoggedGuard } from './guards/user-logged.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books/:start', component: BooksComponent },
+  { path: 'favorites', canActivate: [UserLoggedGuard], component: FavoritesComponent, pathMatch: 'full' },
+  { path: 'sign-in', component: SigninComponent, pathMatch: 'full' },
+  { path: 'sign-up', component: SignupComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
