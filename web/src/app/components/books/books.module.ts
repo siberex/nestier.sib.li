01e8a0cd68dd9b136f57719a9387d012326052b7
Stepan Legachev <siberex@gmail.com>
2019-04-23 02:13:23 +0300

    adding components

--- web/src/app/components/books/books.module.ts
+++ web/src/app/components/books/books.module.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BooksComponent } from './books.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BooksComponent,
+  ],
   imports: [
     CommonModule,
   ],
