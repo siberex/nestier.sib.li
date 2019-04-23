c97e3a6694bd4089cc1d52910b34e5d01f201133
Stepan Legachev <siberex@gmail.com>
2019-04-23 06:16:16 +0300

    refactored component path

--- web/src/app/login/login.module.ts
+++ web/src/app/login/login.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginService } from '../services/login.service';
 import { SigninComponent } from './signin.component';
-import { SignupComponent } from './signup.component';
+import { LoginCreateComponent } from './login-create/login-create.component';
 
 @NgModule({
   declarations: [
-    SignupComponent,
+    LoginCreateComponent,
     SigninComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: '', component: SigninComponent, pathMatch: 'full' },
-      { path: 'create', component: SignupComponent, pathMatch: 'full' },
+      { path: 'create', component: LoginCreateComponent, pathMatch: 'full' },
     ]),
   ],
   providers: [
